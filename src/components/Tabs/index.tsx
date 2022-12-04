70ead36a6250a5407dcd3d3cc497792feee19c29
Steven Thompson <44806974+thompsonsj@users.noreply.github.com>
2022-12-04 14:41:37 +0100

    feat(tabs): add functioning tabbed navigation (#5)

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,75 +1,63 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import stringifyObject from 'stringify-object'
 
 import { SlateValueContext } from "../../contexts/SlateValueContext"
-import { slateToHtml } from "slate-serializers"
+import { htmlToSlate, slateToHtml } from "slate-serializers"
 
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-const tabs = [
-  { name: 'Slate JSON', href: '#', current: true },
-  { name: 'HTML', href: '#', current: false },
-  { name: 'Reserialized Slate', href: '#', current: false },
-]
+import { Tab } from '@headlessui/react'
+import cx from 'classnames'
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+const tabClassnames = cx(
+  "ui-selected:bg-indigo-100",
+  "ui-selected:text-indigo-700",
+  "ui-not-selected:text-gray-500",
+  "ui-not-selected:hover:text-gray-700",
+  "px-3",
+  "py-2",
+  "font-medium",
+  "text-sm",
+  "rounded-md"
+)
 
-export default function Example() {
+function Tabs() {
   const { slateValue } = useContext(SlateValueContext)
+  const [ html, setHtml ] = useState('')
+  const [ reserializedSlate, setReserializedSlate ] = useState([])
+
+  useEffect(() => {
+    setHtml(slateValue ? slateToHtml(JSON.parse(slateValue)): '')
+  }, [slateValue])
+
+  useEffect(() => {
+    setReserializedSlate(html ? htmlToSlate(html): [])
+  }, [html])
 
   return (
-    <>
-    <div>
-      <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
-          Select a tab
-        </label>
-        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-        <select
-          id="tabs"
-          name="tabs"
-          className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-          defaultValue={tabs.find((tab) => tab.current).name}
-        >
-          {tabs.map((tab) => (
-            <option key={tab.name}>{tab.name}</option>
-          ))}
-        </select>
-      </div>
-      <div className="hidden sm:block">
-        <nav className="flex space-x-4" aria-label="Tabs">
-          {tabs.map((tab) => (
-            <a
-              key={tab.name}
-              href={tab.href}
-              className={classNames(
-                tab.current ? 'bg-gray-100 text-gray-700' : 'text-gray-500 hover:text-gray-700',
-                'px-3 py-2 font-medium text-sm rounded-md'
-              )}
-              aria-current={tab.current ? 'page' : undefined}
-            >
-              {tab.name}
-            </a>
-          ))}
-        </nav>
-      </div>
-    </div>
-    <pre><code>{slateValue && JSON.parse(slateValue).map(node => stringifyObject(node)).join('\n')}</code></pre>
-    <pre><code>{slateValue && slateToHtml(JSON.parse(slateValue))}</code></pre>
-    </>
+    <Tab.Group>
+      <Tab.List className="mb-6">
+        <Tab className={tabClassnames}>
+          Slate JSON
+        </Tab>
+        <Tab className={tabClassnames}>
+          HTML
+        </Tab>
+        <Tab className={tabClassnames}>
+          Reserialized Slate JSON
+        </Tab>
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>
+        <pre><code>{slateValue && JSON.parse(slateValue).map(node => stringifyObject(node)).join('\n')}</code></pre>
+        </Tab.Panel>
+        <Tab.Panel>
+        <pre><code>{html}</code></pre>
+        </Tab.Panel>
+        <Tab.Panel>
+        <pre><code>{reserializedSlate && reserializedSlate.map(node => stringifyObject(node)).join('\n')}</code></pre>
+        </Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
   )
 }
+
+export default Tabs
